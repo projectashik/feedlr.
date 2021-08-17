58999fffe58118923ef75cbc3476929bf998a2ac
Ashik Chapagain <projectashik@gmail.com>
2021-08-17 14:33:47 +0545

    Widget Customized

--- public/init.js
+++ public/init.js
@@ -1,1 +1,15 @@
-document.addEventListener("DOMContentLoaded",()=>{const e=document.createElement("link");e.rel="stylesheet",e.href="http://localhost:3000/widget.css",document.head.append(e);const t=document.createElement("iframe");t.src="http://localhost:3000/widget/index.html",t.height="451",t.width="350",t.classList.add("feedlr-widget-iframe-sticky-bottom"),document.body.append(t)});
+document.addEventListener("DOMContentLoaded",()=>{const e=document.createElement("link");e.rel="stylesheet",e.href="http://localhost:3000/widget.css",document.head.append(e);const t=document.createElement("div"),n=document.createElement("iframe");n.src="http://localhost:3000/widget/index.html",n.width="350",n.classList.add("feedlr-widget-iframe-bottom"),n.classList.add("feedlr-widget-sticky-bottom"),n.onload=function(){d();const e=n.contentWindow.document.querySelectorAll(".emoji_radio");e.forEach(e=>{e.addEventListener("change",()=>{d()})})};const o=document.createElement("button");function d(){n.style.height=n.contentWindow.document.body.scrollHeight+10+"px"}o.innerHTML=`<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="feather feather-x"
+>
+  <line x1="18" y1="6" x2="6" y2="18"></line>
+  <line x1="6" y1="6" x2="18" y2="18"></line>
+</svg>`,o.classList.add("feedlr-widget-close_button"),o.onclick=function(){document.body.removeChild(t)},t.append(n),t.append(o),document.body.append(n),d()});
--- scripts/init.js
+++ scripts/init.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const url = 'http://localhost:3000';
   const iframeUrl = url + '/widget/index.html';
   const styleUrl = url + '/widget.css';
+
   // Creating link tag
   const linkTag = document.createElement('link');
   linkTag.rel = 'stylesheet';
@@ -10,13 +11,63 @@ document.addEventListener('DOMContentLoaded', () => {
   // Injecting link tag
   document.head.append(linkTag);
 
+  //
+  const container = document.createElement('div');
+
   // Creating Iframe Tag
   const iframe = document.createElement('iframe');
   iframe.src = iframeUrl;
-  iframe.height = '451';
   iframe.width = '350';
-  iframe.classList.add('feedlr-widget-iframe-sticky-bottom');
+  iframe.classList.add('feedlr-widget-iframe-bottom');
+  iframe.classList.add('feedlr-widget-sticky-bottom');
+  iframe.onload = function () {
+    changeIframeHeight();
 
-  // Injecting Iframe
-  document.body.append(iframe);
+    const emojis_buttons =
+      iframe.contentWindow.document.querySelectorAll('.emoji_radio');
+    emojis_buttons.forEach((btn) => {
+      btn.addEventListener('change', () => {
+        changeIframeHeight();
+      });
+    });
+  };
+
+  // Create Close Button
+  const button = document.createElement('button');
+  button.innerHTML = `<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="feather feather-x"
+>
+  <line x1="18" y1="6" x2="6" y2="18"></line>
+  <line x1="6" y1="6" x2="18" y2="18"></line>
+</svg>`;
+  button.classList.add('feedlr-widget-close_button');
+  button.onclick = closeModal;
+
+  function openModal() {
+    container.append(iframe);
+    container.append(button);
+    // Injecting Iframe
+    document.body.append(iframe);
+  }
+
+  function closeModal() {
+    document.body.removeChild(container);
+  }
+
+  function changeIframeHeight() {
+    iframe.style.height =
+      iframe.contentWindow.document.body.scrollHeight + 10 + 'px';
+  }
+
+  openModal();
+  changeIframeHeight();
 });
